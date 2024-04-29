98a6129f655d194893f78990e434ba4b0d0a854b
Nicholas Zani <nczani@gmail.com>
2024-04-29 14:09:54 -0300

    add images with enhanced quality, improve navbar selected item

--- app.js
+++ app.js
@@ -21,11 +21,31 @@ window.addEventListener("load", adjustTextareaHeight);
 //Ajustar a altura quando a página mudar de tamanho
 window.addEventListener("resize", adjustTextareaHeight);
 
-//form
-window.addEventListener("DOMContentLoaded", function () {
-  var form = document.getElementById("form-id");
+//Mudar cor do nav-item de acordo com a página aberta
 
-  document.getElementById("a-submit").addEventListener("click", function () {
-    form.submit();
-  });
+window.addEventListener("DOMContentLoaded", function selectNavItem() {
+  previousPage = document.querySelector(".selected");
+  if (previousPage) {
+    previousPage.classList.remove("selected");
+  }
+  if (previousPage == "about2") {
+    document.getElementById("cv").classList.remove("selected");
+    document.getElementById("about").classList.remove("selected");
+  }
+  const body = document.querySelector("body");
+  const currentPage = body.className;
+  const element = document.getElementById(currentPage);
+  element.classList.add("selected");
+  if (currentPage == "about" || currentPage == "cv") {
+    document.getElementById("about2").classList.add("selected");
+  }
 });
+
+//form
+// window.addEventListener("DOMContentLoaded", function () {
+//   var form = document.getElementById("form-id");
+
+//   document.getElementById("a-submit").addEventListener("click", function () {
+//     form.submit();
+//   });
+// });
